9516b243a51f941c4a6bab8f8c719d7f041ed467
asghar <you@example.com>
2024-02-14 11:44:03 +0530

    commit: java file Added

---
+++ AasScan.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String[] args) {
+        int month_days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        String month_names[] = {"jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"};
+       
+       Scanner umar = new Scanner(System.in);
+       System.out.println("Enter month_no one by one from 1 - 12");
+        for (int i=0; i<12; i++) {
+
+int myint = umar.nextInt();
+    
+System.out.println(month_names[i] + " is having " + month_days[i] + " days");
+
+
+
+    }
+
+ 
+        }
+
+
+}
+
+
+https://oit.ncsu.edu/wp-content/uploads/2015/11/weill_ross_framework_pdf_99126.pdf
+
+https://download.e-bookshelf.de/download/0000/5842/12/L-G-0000584212-0002384276.pdf
+
+Information Technology (IT) for Business 
+Business Essentials Eleventh Edition elbirt & griffin
+
+----------------------------------
+syllabus:
+Week 1: Introduction to IT Applications for Business
+
+Overview of IT applications in business
+Importance of IT alignment with business strategy
+Trends and challenges in IT adoption
+Week 2-3: Enterprise Systems
+
+Introduction to enterprise resource planning (ERP) systems
+Implementation and customization of ERP systems
+Benefits and challenges of ERP adoption
+Case studies on successful ERP implementations
+Week 4-5: Business Intelligence and Analytics
+
+Fundamentals of business intelligence (BI) and analytics
+Data-driven decision-making
+Data visualization techniques
+Predictive analytics and machine learning
+Week 6-7: E-Commerce and Digital Business Models
+
+Overview of e-commerce technologies and platforms
+Business models in the digital economy
+Online retailing, digital marketing, and customer relationship management (CRM)
+Legal and ethical considerations in e-commerce
+Week 8-9: Emerging Technologies
+
+Introduction to emerging technologies such as blockchain, Internet of Things (IoT), and artificial intelligence (AI)
+Applications of emerging technologies in business
+Opportunities and challenges of adopting emerging technologies
+Week 10: IT Governance and Strategy
+
+IT governance frameworks (e.g., COBIT, ITIL)
+Strategic alignment of IT with business objectives
+IT project management and risk assessment
+Regulatory compliance and cybersecurity
+Week 11-12: Case Studies and Project Presentations
+
+Analysis of real-world case studies illustrating IT applications in various industries
+Group project presentations on innovative IT solutions for business challenges
+Reflections on lessons learned and future trends in IT applications for business
+------------------------------------------------
+unit-1:
+https://www.oregon.gov/eis/Documents/IT_Governance_Guide_EIS.pdf
+https://ddceutkal.ac.in/Syllabus/MBA-BOOK/Corporate-Governance.pdf
+https://itgov.cs.ui.ac.id/itgov/IT%20gov%20intro.pdf
+https://www.icsi.edu/media/webmodules/GOVERNANCE_RISK_MANAGEMENT_COMPLIANCES_AND_ETHICS.pdf
+https://www.techtarget.com/searchsecurity/definition/governance-risk-management-and-compliance-GRC
+https://mu.ac.in/wp-content/uploads/2022/06/Information-Technology-for-Management-1.pdf
+
+additional: 
+https://books.google.co.in/books?id=al6zP7foCSEC&printsec=copyright&redir_esc=y#v=onepage&q&f=false
+https://managementstudyguide.com/risk-management-information-system.htm
+
+
+unit-2:
+https://www.business.qld.gov.au/running-business/digital-business/online-risk-security/risk#defining-it-risk
+https://flevy.com/browse/business-document/it-strategy-209
+https://www.nibusinessinfo.co.uk/content/align-it-your-business-strategy
+
+https://encyclopedia.pub/entry/17116
+
+
+unit-3:
+http://mitp-content-server.mit.edu:18180/books/content/sectbyfn?collid=books_pres_0&id=8811&fn=9780262015387_sch_0001_0.pdf
+https://nap.nationalacademies.org/read/6322/chapter/2#2
+https://genuinenotes.com/wp-content/uploads/2021/05/Unit-1-Information-Systems-in-Global-Business-Today-1.pdf
+
+
+Additional: https://en.wikipedia.org/wiki/Information_system
+
+
+unit-4:
+https://pressbooks.pub/bus206/chapter/chapter-6-information-systems-security/
+http://eprints.dinus.ac.id/14397/1/[Materi]_laudon_-_management_Information_Systems_12_-_Chapter_8_SECURING_INFORMATION_SYSTEMS.pdf
+https://jumpcloud.com/blog/what-are-it-general-controls-itgc
+https://www.in.gov/sboa/files/CH14-Information-Technology-Controls.pdf
+
+
+unit5:
+https://police.py.gov.in/Information%20Technology%20Act%202000%20-%202008%20(amendment).pdf
+http://www.hja.legal/wp-content/uploads/2015/05/Clause-49-of-the-Listing-Agreement-as-Amended.pdf
+
+unit6:
+https://www.stannescet.ac.in/cms/staff/qbank/CSE/Notes/CS6004-CYBER%20FORENSICS-1800235714-CS6004%20UNIT%203.pdf
+https://annamalaiuniversity.ac.in/studport/download/engg/it/resources/Cyber%20Forensics.pdf
+https://www.tndalu.ac.in/econtent/27_Information_Security_And_Digital_Forensics.pdf
+https://www.itgovernance.co.uk/files/infosec_101v1.1.pdf
+
+https://hightable.io/iso-27001-information-security-policy/
+https://www.ifac.org/_flysystem/azure-private/publications/files/B007%202013%20IAASB%20Handbook%20ISAE%203402_0.pdf
+https://itgov.cs.ui.ac.id/itgov/IT%20Infrastructure%20Library.pdf
+
+
+unit-7
+https://afsaonline.org/wp-content/uploads/2021/07/Business-Continuity-Management-IT-Booklet.pdf
+https://www.ucf.edu/online/leadership-management/news/business-continuity-vs-disaster-recovery/#:~:text=Business%20continuity%20planning%20may%20also,terror%2C%20active%20shooter%20or%20cybercrime.
+
+
+
+
+
+
+
+
+
+
